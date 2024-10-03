bfc4aa9c2eb782ba2c87203ac5159e08dd9cf1ae
Irham Dzuhri <irhamdz@gmail.com>
2024-10-03 19:30:26 +0700

    fix bot always stop and fix for many accounts

--- bot/config/config.py
+++ bot/config/config.py
@@ -11,7 +11,7 @@ class Settings:
     MIN_AVAILABLE_ENERGY = int(os.getenv("MIN_AVAILABLE_ENERGY", '100'))
     SLEEP_BY_MIN_ENERGY = json.loads(os.getenv("SLEEP_BY_MIN_ENERGY", '[1800, 3600]'))
 
-    ADD_TAPS_ON_TURBO = int(os.getenv("ADD_TAPS_ON_TURBO", '2500'))
+    ADD_TAPS_ON_TURBO = json.loads(os.getenv("ADD_TAPS_ON_TURBO", '[50000, 100000]'))
 
     AUTO_UPGRADE_TAP = os.getenv("AUTO_UPGRADE_TAP", 'True')
     MAX_TAP_LEVEL = int(os.getenv("MAX_TAP_LEVEL", '5'))
@@ -26,13 +26,14 @@ class Settings:
     AUTO_CLEAR_MISSION = os.getenv("AUTO_CLEAR_MISSION", 'True')
     AUTO_PLAY_SPIN = os.getenv("AUTO_PLAY_SPIN", 'False')
 
-    RANDOM_TAPS_COUNT = json.loads(os.getenv("RANDOM_TAPS_COUNT", '[15, 75]'))
     SLEEP_BETWEEN_TAP = json.loads(os.getenv("SLEEP_BETWEEN_TAP", '[15, 60]'))
     ACTIVE_TURBO_DELAY = int(os.getenv("ACTIVE_TURBO_DELAY", '4'))
 
     USE_PROXY_FROM_FILE = os.getenv("USE_PROXY_FROM_FILE", 'False')
 
     USE_TAP_BOT = os.getenv("USE_TAP_BOT", 'True')
+    DELAY_EACH_ACCOUNT = json.loads(os.getenv("DELAY_EACH_ACCOUNT", "[10, 15]"))
+    DELAY_ALL_ACCOUNT = json.loads(os.getenv("DELAY_ALL_ACCOUNT", "[3600, 5000]"))
 
 
 settings = Settings()
--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,40 +1,29 @@
+import os
+
 from bot.utils import logger
 from helpers import get_tele_user_obj_from_query_id
 
 
-async def register_query_id() -> None:
-
+async def register_query_id():
+    query_file_name = "query_ids.txt"
     add = True
     while add:
-        query_id_str = input("\nPlease input query id (press Enter to exit): ")
+        query_id = input("\nPlease input query id (press Enter to exit): ")
 
-        if not query_id_str:
+        if not query_id:
             break
-        
-        tele_user_obj = get_tele_user_obj_from_query_id(query_id_str)
+
+        tele_user_obj = get_tele_user_obj_from_query_id(query_id)
         username = tele_user_obj.get("username")
         first_name = tele_user_obj.get("first_name")
         last_name = tele_user_obj.get("last_name")
-        
-        username_exist = False
-        
-        with open("query_ids.txt", "r+") as fd:
-            content = fd.readlines()
-            if content:
-                existing_username = []
-                for cnt in content:
-                    cnt_user_obj = get_tele_user_obj_from_query_id(cnt)
-                    cnt_username = cnt_user_obj.get("username")
-                    existing_username.append(cnt_username)
 
-                if username in set(existing_username):
-                    username_exist = True
-                else:
-                    fd.write(f"\n{query_id_str.strip()}")
-            else:
-                fd.write(f"{query_id_str.strip()}")
-
-        if username_exist:
-            logger.error(f"Session @{username} already added, please use other account")
+        if os.path.exists(query_file_name):
+            # Open the file in append mode and add the input value
+            with open(query_file_name, "a") as file:
+                file.write(query_id + "\n")
+                logger.success(
+                    f"Successfully added @{username} | {first_name} {last_name} to {query_file_name}"
+                )
         else:
-            logger.success(f"Successfully added @{username} | {first_name} {last_name}")
+            logger.success(f"{query_file_name} does not exist.")
